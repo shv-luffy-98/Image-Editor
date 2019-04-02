0c5233d8c3b899c2708c77ee8d5215ce5d1dd62e
Luffy <shvmain69@gmail.com>
2019-04-03 01:12:44 +0530

    completed basic filters and ui

--- app/src/main/java/com/example/shv/imageeditor/EditorView.java
+++ app/src/main/java/com/example/shv/imageeditor/EditorView.java
@@ -1,20 +1,41 @@
 package com.example.shv.imageeditor;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import static com.example.shv.imageeditor.MainActivity.selectedImage;
+
 public class EditorView extends AppCompatActivity {
+    public Button apply, save;
     private List<Filter> filters;
     private RecyclerView thumbListView;
     private ImageView originalView;
@@ -25,6 +46,8 @@ public class EditorView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor_view);
         originalView = (ImageView) findViewById(R.id.originalImage);
+        apply = (Button) findViewById(R.id.Apply);
+        save = (Button) findViewById(R.id.Save);
 
         filters = createFilters();
         thumbListView = (RecyclerView) findViewById(R.id.filterRecyclerView);
@@ -39,11 +62,13 @@ public class EditorView extends AppCompatActivity {
         thumbListView.addOnItemTouchListener(new CustomRVItemTouchListener(this, thumbListView, new RecyclerViewItemClickListener() {
             @Override
             public void onClick(View view, int position) {
-                Toast.makeText(getBaseContext(), Integer.toString(position), Toast.LENGTH_LONG).show();
-                if(position == 0)
+                if (position == 0) {
                     EditorView.this.imageDisplay();
-                else
+                    EditorView.this.apply.setEnabled(false);
+                } else {
                     EditorView.this.applyFilter(position);
+                    EditorView.this.apply.setEnabled(true);
+                }
             }
 
             @Override
@@ -53,38 +78,71 @@ public class EditorView extends AppCompatActivity {
         }));
         initImage();
     }
-    private void initImage(){
-        String currentPhotoPath = getIntent().getStringExtra("photoPath");
 
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-        int scaleFactor = Math.min(photoW/300, photoH/400);
+    private void initImage() {
+        if (getIntent().getStringExtra("option").equals("1")) {
+            String currentPhotoPath = getIntent().getStringExtra("photoPath");
 
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-        originalImage = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
-        imageDisplay();
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+            int photoW = bmOptions.outWidth;
+            int photoH = bmOptions.outHeight;
+            int scaleFactor = Math.min(photoW / 300, photoH / 400);
+
+            bmOptions.inJustDecodeBounds = false;
+            bmOptions.inSampleSize = scaleFactor;
+            originalImage = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+            imageDisplay();
+        } else {
+            Bitmap bmp = null;
+            try {
+                bmp = getBitmapFromUri(selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            originalImage = Bitmap.createBitmap(bmp);
+
+            imageDisplay();
+        }
     }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+
+
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+
+        int height = image.getHeight();
+        int width = image.getWidth();
+        double scale = Math.min(300.0 / width, 400.0 / height);
+
+        Matrix mat = new Matrix();
+        mat.postScale((float) scale, (float) scale);
+        Bitmap resizedBitmap = Bitmap.createBitmap(image, 0, 0, width, height, mat, false);
+        return resizedBitmap;
+    }
+
     public void applyFilter(int position) {
         int height = originalImage.getHeight();
         int width = originalImage.getWidth();
         int matrix[][] = filters.get(position).filterMatrix;
         int size = filters.get(position).size;
         int factor = filters.get(position).factor;
-        int s2 = size/2;
+        int s2 = size / 2;
 
         Bitmap temp = Bitmap.createBitmap(width, height, originalImage.getConfig());
         int A, R, G, B;
         int sumR, sumG, sumB;
         int[][] pixels = new int[size][size];
 
-        for(int y = s2; y < height - s2; ++y) {
-            for(int x = s2; x < width - s2; ++x) {
-                for(int i = 0; i < size; ++i) {
-                    for(int j = 0; j < size; ++j) {
+        for (int y = s2; y < height - s2; ++y) {
+            for (int x = s2; x < width - s2; ++x) {
+                for (int i = 0; i < size; ++i) {
+                    for (int j = 0; j < size; ++j) {
                         pixels[i][j] = originalImage.getPixel(x + i - s2, y + j - s2);
                     }
                 }
@@ -92,8 +150,8 @@ public class EditorView extends AppCompatActivity {
                 sumR = 0;
                 sumG = 0;
                 sumB = 0;
-                for(int i = 0; i < size; ++i) {
-                    for(int j = 0; j < size; ++j) {
+                for (int i = 0; i < size; ++i) {
+                    for (int j = 0; j < size; ++j) {
                         sumR += Color.red(pixels[i][j]) * matrix[i][j];
                         sumG += Color.green(pixels[i][j]) * matrix[i][j];
                         sumB += Color.blue(pixels[i][j]) * matrix[i][j];
@@ -104,84 +162,161 @@ public class EditorView extends AppCompatActivity {
                 G = sumG / factor;
                 B = sumB / factor;
 
-                if(position == 6) {
+                if (position == 6) {
                     R += 128;
                     G += 128;
                     B += 128;
                 }
 
-                if(R < 0) R = 0;
-                else if(R > 255) R = 255;
+                if (R < 0) R = 0;
+                else if (R > 255) R = 255;
 
-                if(G < 0) G = 0;
-                else if(G > 255) G = 255;
+                if (G < 0) G = 0;
+                else if (G > 255) G = 255;
 
-                if(B < 0) B = 0;
-                else if(B > 255) B = 255;
-                temp.setPixel(x , y, Color.argb(A, R, G, B));
+                if (B < 0) B = 0;
+                else if (B > 255) B = 255;
+                temp.setPixel(x, y, Color.argb(A, R, G, B));
             }
         }
         imageDisplay(temp);
         tempImage = temp;
     }
 
-    public void imageDisplay(Bitmap image){
+    public void imageDisplay(Bitmap image) {
         originalView.setImageBitmap(image);
     }
-    public void imageDisplay(){
+
+    public void imageDisplay() {
         originalView.setImageBitmap(originalImage);
+    }
+
+    public void applyChanges(View view) {
+        originalImage = Bitmap.createBitmap(tempImage);
+        save.setEnabled(true);
+        apply.setEnabled(false);
+    }
+
+    public void saveChanges(View view) {
+        if (storagePermissionGranted()) {
+            storeImage();
+        }
+    }
+
+    public boolean storagePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED)
+                return true;
+            else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            EditorView.this.storeImage();
+        else
+            Toast.makeText(getApplicationContext(), "Permission denied", Toast.LENGTH_LONG).show();
+    }
+
+
+    private void storeImage() {
+        Bitmap image = Bitmap.createBitmap(originalImage);
+        File pictureFile = getOutputMediaFile();
+        if (pictureFile == null) {
+            Toast.makeText(getApplicationContext()
+                    , "Error creating media file, check storage permissions: ", Toast.LENGTH_LONG).show();
+            return;
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            image.compress(Bitmap.CompressFormat.PNG, 90, fos);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Toast.makeText(getApplicationContext()
+                    , "File not found: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            return;
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext()
+                    , "Error accessing file: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            return;
+        }
+        Toast.makeText(getApplicationContext(), "Image saved in " + pictureFile.getAbsolutePath(), Toast.LENGTH_LONG).show();
+        save.setEnabled(false);
+    }
+
+    private File getOutputMediaFile() {
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory()
+                + "/Android/data/"
+                + getApplicationContext().getPackageName()
+                + "/Pictures");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+        String timeStamp = new SimpleDateFormat("ddMMyyyy_HHmm").format(new Date());
+        File mediaFile;
+        String mImageName = "MI_" + timeStamp + ".jpg";
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + mImageName);
+        return mediaFile;
     }
 
     public List<Filter> createFilters() {
         List<Filter> filters = new ArrayList<>();
-        int a[][] = new int[][] {
+        int a[][] = new int[][]{
                 {0, 0, 0},
                 {0, 1, 0},
                 {0, 0, 0}
         };
         filters.add(new Filter("Normal", a, 3, 1));
 
-        a = new int[][] {
-            {1, 1, 1},
-            {1, 1, 1},
-            {1, 1, 1}
+        a = new int[][]{
+                {1, 1, 1},
+                {1, 1, 1},
+                {1, 1, 1}
         };
-        filters.add(new Filter("Blur 3x3", a, 3, 9));
+        filters.add(new Filter("Gauss Blur 1", a, 3, 9));
 
-        a = new int[][] {
+        a = new int[][]{
                 {1, 1, 1, 1, 1},
                 {1, 1, 1, 1, 1},
                 {1, 1, 1, 1, 1},
                 {1, 1, 1, 1, 1},
                 {1, 1, 1, 1, 1}
         };
-        filters.add(new Filter("Blur 5x5", a, 5, 25));
+        filters.add(new Filter("Gauss Blur 2", a, 5, 25));
 
-        a = new int[][] {
+        a = new int[][]{
                 {-1, -1, -1},
                 {-1, 8, -1},
                 {-1, -1, -1}
         };
         filters.add(new Filter("Edge Detect", a, 3, 1));
 
-        a = new int[][] {
+        a = new int[][]{
                 {1, 0, 0},
                 {0, 1, 0},
                 {0, 0, 1}
         };
         filters.add(new Filter("Motion Blur", a, 3, 3));
 
-        a = new int[][] {
+        a = new int[][]{
                 {-1, -1, -1},
-                {-1,  9, -1},
+                {-1, 9, -1},
                 {-1, -1, -1}
         };
         filters.add(new Filter("Sharpen", a, 3, 1));
 
-        a = new int[][] {
-                {-1, -1,  0},
-                {-1,  0,  1},
-                {0,  1,  1}
+        a = new int[][]{
+                {-1, -1, 0},
+                {-1, 0, 1},
+                {0, 1, 1}
         };
         filters.add(new Filter("Emboss", a, 3, 1));
         return filters;
--- app/src/main/java/com/example/shv/imageeditor/MainActivity.java
+++ app/src/main/java/com/example/shv/imageeditor/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.shv.imageeditor;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
@@ -14,13 +16,16 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileDescriptor;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int RESULT_LOAD_IMAGE = 2;
     public String currentPhotoPath;
+    public static Uri selectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,14 +47,34 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+    public void selectImage(View view) {
+        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK ) {
             Intent intent = new Intent(getBaseContext(), EditorView.class);
+            intent.putExtra("option", "1");
             intent.putExtra("photoPath", currentPhotoPath);
             startActivity(intent);
         }
+        else if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn,
+                    null, null, null);
+            cursor.moveToFirst();
+            cursor.close();
+
+            Intent intent = new Intent(getBaseContext(), EditorView.class);
+            intent.putExtra("option","2");
+            startActivity(intent);
+        }
     }
+
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
